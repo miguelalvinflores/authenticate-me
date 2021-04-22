ea5fc79af15975557c7f7332f233070a17073562
miguel <miguelalvinflores@gmail.com>
2021-04-22 15:10:14 -0700

    Logout feature

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -27,4 +27,12 @@ router.post('/', asyncHandler(async (req, res, next) => {
 }),
 );
 
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
 module.exports = router;
