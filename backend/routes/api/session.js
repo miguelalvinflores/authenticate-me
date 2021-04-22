8bfb8e727d938e0dd4bd578bb8dcc2f8ded3e44a
miguel <miguelalvinflores@gmail.com>
2021-04-22 15:43:06 -0700

    completed backend template

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
-const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
