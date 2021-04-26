112ed05a01e69e03afb8df09c5d74c44cbb0e776
miguel <miguelalvinflores@gmail.com>
2021-04-25 20:11:58 -0700

    signup form completed

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -33,6 +33,20 @@ export const login = (user) => async (dispatch) => {
   return res;
 }
 
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const response = await csrfFetch("api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setSessionUser(data.user));
+  return response;
+};
 
 
 export default function sessionReducer(state = { user: null}, action) {
